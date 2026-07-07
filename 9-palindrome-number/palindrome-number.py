40d6cd18d807968fb0896f992b05b4a5a767f62d
Tanaya_V <tanaya.vaidya25@gmail.com>
2026-07-07 18:42:48 +0530

    Time: 4 ms (80.55%) | Memory: 19.4 MB (17.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,11 +1,15 @@
-class Solution:
+'''class Solution:
     def isPalindrome(self, x: int) -> bool:
         s = str(x)
         rev=s[::-1]
         if(s==rev):
             return True
         else:
-            return False
+            return False'''
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        return s == s[::-1]
 
 
         
